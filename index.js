1ef44d7d3dae8612fe9c3be0a0d34d6c46fa0da4
awadh <awadh.hassan@student.moringaschool.com>
2022-08-10 00:06:52 +0300

    a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.right.replace("px", "");
+    const right = parseInt(RightNumbers, 10);
+  
+    if (right > 0) {
+      dodger.style.right = `${right - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
