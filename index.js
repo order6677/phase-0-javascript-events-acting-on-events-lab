fbad778e0dd62acc260e832b4c3f6b66124c5472
awadh <awadh.hassan@student.moringaschool.com>
2022-08-22 21:44:55 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById("dodger"); 
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-  
+
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
-  }
+  } 
 
-document.addEventListener("keydown", function (e) {
+  document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
-  });
-  function moveDodgerRight() {
-    const RightNumbers = dodger.style.right.replace("px", "");
-    const right = parseInt(RightNumbers, 10);
-  
-    if (right > 0) {
-      dodger.style.right = `${right - 1}px`;
+  }); 
+
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+
     }
+
   }
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
+  document.addEventListener("keydown", function(e){
+
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
     }
   });
